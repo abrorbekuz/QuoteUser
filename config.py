ccfe10703548af9a58c8c063acbeca6569b5cad2
Abror <69087966+abrorbekuz@users.noreply.github.com>
2022-08-02 15:58:23 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 
 details = {
-    "name" : "poco", "api_id" : 5976123, "api_hash" : "daecbecef56cf6f3b2a2e0e36f49fa03",
-    "session_string" : "AgBbMDsAdzUHbWH4z7e1C2Qt_o6MisBi3qgE78b6O-CJv559_uwTxhy5JofLXgY4BwMCY8baqFGPPw1BwQMHFgn91UMvJrscAbFgJKBc0PgqtOZSxGFLZH5xLrCyLZWBOI5hTjo7UMPF9J-DemixrFypz9O0YqHvZeGZqZuDpi4OPcJAEQsMSFGs7NmfCS5HLwBfxsX7lC0qAYPBjQp3Xh-dS2U6-v1LlvXsVgARgGkAWmobq_pfWJR8mR9mPnuv6imRj1NUpJGHzySRpONu0i0tFoADTC21F3ac6PW9fsVrqAyDIACXOyfPw19Bib-Pmw49AxVVNDkr97cgtrIs7gSzKg8HdAAAAABuWBOVAA",
+    "name" : "chupakabra",
+    "api_id" : "<your_api_id>",
+    "api_hash" : "<your_api_hash>",
+    "session_string" : "<your_string_session>",
 }
